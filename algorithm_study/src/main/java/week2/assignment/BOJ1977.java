2c4a6e22a5a1bf1a2ef366741731ddf36d56b80c
HMYIEN <bsm0407@naver.com>
2025-02-18 22:56:52 +0900

    [브루트포스 알고리즘] BOJ1977 (자바 모듈 8로 안바꾼듯)
    완전제곱수

--- algorithm_study/src/main/java/week2/assignment/BOJ1977.java
+++ algorithm_study/src/main/java/week2/assignment/BOJ1977.java
@@ -3,27 +3,26 @@ package week2.assignment;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class BOJ1977 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int m = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
-        int i = 1;
         int sum = 0;
         int min = Integer.MAX_VALUE;
 
-        while (true) {
-            if (m < Math.sqrt(i) && Math.sqrt(i) <= n) {
-                if (min > Math.sqrt(i)) {
-                    min = (int)Math.sqrt(i);
-                }
-                sum += (int)Math.sqrt(i);
-            } else continue;
-            i++;
+        for (int i = m; i <= n; i++) {
+            if (Math.sqrt(i) % 1 == 0) {
+                sum += i;
+                min = Math.min(min, sum);
+            }
         }
 
-        System.out.println(min);
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum + "\n" + min);
+        }
     }
 }

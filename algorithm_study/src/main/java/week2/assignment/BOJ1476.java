1e8d19508ac293d37ec511a2ec54ead455ea1d7b
HMYIEN <bsm0407@naver.com>
2025-02-20 01:03:19 +0900

    [브루트포스 알고리즘] BOJ1476
    날짜 계산

--- algorithm_study/src/main/java/week2/assignment/BOJ1476.java
+++ algorithm_study/src/main/java/week2/assignment/BOJ1476.java
@@ -1,4 +1,25 @@
 package week2.assignment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ1476 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int i = E;
+        while (true) {
+            if ((i - E) % 15 == 0 && (i - S) % 28 == 0 && (i - M) % 19 == 0) {
+                System.out.println(i);
+                return;
+            }
+            i += 15;
+        }
+    }
 }

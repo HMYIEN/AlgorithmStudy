5bab91cfd3eb18e6c74c2f0d65175f4917698f7b
HMYIEN <bsm0407@naver.com>
2025-02-27 01:02:01 +0900

    A->B 푸는중..

---
+++ algorithm_study/src/main/java/week3/assignment/BOJ16953.java
@@ -0,0 +1,26 @@
+package week3.assignment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ16953 {
+    static Long A, B;
+    static Queue<Long> q = new LinkedList<>();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+
+        System.out.println(bfs(A, B));
+    }
+
+    private static int bfs(Long a, Long b) {
+    }
+}
